45cdaf002a908a296ecc78f57ec857ec5852a755
Pascal Zittlau <pascal.zittlau@gmail.com>
2026-03-26 13:57:05 +0100

    migrate LRUCachePolicy to the O(1) LinkedKeySet removing linear scans

--- betree/src/cache/lru_policy.rs
+++ betree/src/cache/lru_policy.rs
@@ -2,22 +2,22 @@
 //!
 //! LRU is implemented on a doubly linked list.
 
-use parking_lot::Mutex;
-use std::{collections::LinkedList, hash::Hash};
-
+use crate::cache::linked_list::LinkedKeySet;
 use crate::cache::{cache_policy::CachePolicy, CacheAccess, RemoveError};
+use parking_lot::Mutex;
+use std::hash::Hash;
 
-/// Implements an LRU Cache Policy ontop of a doubly linked list.
-/// Keys are added at the front. On eviction, the tail is evicted.
+/// Implements an LRU Cache Policy on top of a highly efficient O(1) doubly linked list.
+/// Keys are added at the front (MRU). On eviction, the tail (LRU) is evicted.
 pub struct LRUCachePolicy<K> {
-    lru_list: Mutex<LinkedList<K>>,
+    lru_list: Mutex<LinkedKeySet<K>>,
 }
 
-impl<K: Hash + Eq> LRUCachePolicy<K> {
+impl<K: Clone + Hash + Eq> LRUCachePolicy<K> {
     /// Returns new cache instance with the given `capacity`.
     pub fn new() -> Self {
         LRUCachePolicy {
-            lru_list: Mutex::new(Default::default()),
+            lru_list: Mutex::new(LinkedKeySet::new()),
         }
     }
 }
@@ -31,16 +31,12 @@ impl<K: Clone + Eq + Hash + Send + Sync + 'static> CachePolicy<K> for LRUCachePo
         return self.lru_list.lock().len();
     }
 
-    // TODO: We may want to implement the lock-free queue from this paper in the future if we need
-    // more performance:
-    // "Simple, Fast, and Practical Non-Blocking and Blocking Concurrent Queue Algorithms"
-    // by Maged M. Michael, Michael L. Scott
     fn on_access(&self, accessed_key: &K, _access: CacheAccess) {
         let mut list = self.lru_list.lock();
-        match list.extract_if(|k| k == accessed_key).nth(0) {
-            Some(k) => list.push_front(k),
-            None => (),
-        };
+        if list.contains(accessed_key) {
+            // push_front moves the item to the MRU position if it exists
+            list.push_front(accessed_key.clone());
+        }
     }
 
     fn on_add(&mut self, added_key: K) {
@@ -48,20 +44,16 @@ impl<K: Clone + Eq + Hash + Send + Sync + 'static> CachePolicy<K> for LRUCachePo
     }
 
     fn on_remove(&mut self, removed_key: &K) -> Option<RemoveError> {
-        match self
-            .lru_list
-            .get_mut()
-            .extract_if(|k| k == removed_key)
-            .count()
-        {
-            0 => Some(RemoveError::NotPresent),
-            _ => None,
+        if self.lru_list.get_mut().remove(removed_key) {
+            None
+        } else {
+            Some(RemoveError::NotPresent)
         }
     }
 
     fn update(&mut self, old_key: &K, new_key: K) {
         let list = self.lru_list.get_mut();
-        _ = list.extract_if(|k| k == old_key);
+        list.remove(old_key);
         list.push_front(new_key);
     }
 
@@ -69,7 +61,8 @@ impl<K: Clone + Eq + Hash + Send + Sync + 'static> CachePolicy<K> for LRUCachePo
         &mut self,
         f: &mut dyn FnMut(&K) -> Option<usize>,
     ) -> Option<(K, usize)> {
-        for key in self.lru_list.get_mut().iter().rev() {
+        let list = self.lru_list.get_mut();
+        for key in list.iter_lru() {
             if let Some(size) = f(key) {
                 return Some((key.clone(), size));
             }
